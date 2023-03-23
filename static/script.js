39f73b89b1eff157b2dbb43db761d8fbc083f6c1
Matthew Long <longm7676@gmail.com>
2023-03-23 16:09:12 -0500

    added spinner while api searches price history

--- static/script.js
+++ static/script.js
@@ -1,37 +1,46 @@
 const $input = $("#player_count");
 const $table = $("#formtable");
+const $td = $("#td");
+const $spinner = $("#spinner");
+$spinner.hide()
+
 
 function generateForms() {
   const cells = 2;
   const players = $input.val();
   const $table = $("#formtable");
   const body = document.createElement("tbody");
-//   console.log(players);
-  for (let x = 0; x < players -1; x++) {
-    const head = document.createElement('th')
-    head.setAttribute('scope', 'row');
-    head.setAttribute('name', `${x + 2}`)
+  //   console.log(players);
+  for (let x = 0; x < players - 1; x++) {
+    const head = document.createElement("th");
+    head.setAttribute("scope", "row");
+    head.setAttribute("name", `${x + 2}`);
     head.innerHTML = x + 2;
     const row = document.createElement("tr");
-    row.append(head)
+    row.append(head);
     row.classList.add("border");
     for (let y = 0; y < cells; y++) {
       const cell = document.createElement("td");
       const input = document.createElement("input");
-      input.setAttribute('name',`row${x + 2}input${y + 1}`)
+      input.setAttribute("name", `row${x + 2}input${y + 1}`);
       input.classList.add("form-control");
       cell.classList.add("border");
       cell.append(input);
       row.append(cell);
     }
-   
-    head.setAttribute
+
+    head.setAttribute;
     body.append(row);
     $table.append(body);
   }
-  $input.hide()
+  $input.hide();
 }
 
 $input.change(function () {
   generateForms();
+});
+
+$('tr').on('click', '#td', function() {
+  $spinner.show();
+  $('#valueplace').hide();
 });
